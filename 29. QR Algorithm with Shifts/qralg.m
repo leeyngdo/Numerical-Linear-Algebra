7f12ea3f9d2ee766c42ef4e48f85f9287f010850
Youngdo Lee <lyd0531@kaist.ac.kr>
2022-05-29 00:22:57 +0900

    Exercise 29 (b)
    Unshifted QR Algorithm

---
+++ 29. QR Algorithm with Shifts/qralg.m
@@ -0,0 +1,9 @@
+%% Input: Let T be the tridiagonal symmetric matrix
+%% Output: Current matrix of T running unshifted QR Algorithm
+function Tnew = qralg(T)
+[m, ~] = size(T);
+while abs(T(m, m-1)) >= 10^(-12)
+    [Qk, Rk] = qr(T); % For greater efficiency
+    T = Rk * Qk; 
+end
+Tnew = T; % output 
