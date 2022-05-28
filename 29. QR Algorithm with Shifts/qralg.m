f73994e745e219b120fd3ef28c862acbcb3c9d53
Youngdo Lee <lyd0531@kaist.ac.kr>
2022-05-29 00:56:30 +0900

    Add Indentation

--- 29. QR Algorithm with Shifts/qralg.m
+++ 29. QR Algorithm with Shifts/qralg.m
@@ -1,9 +1,10 @@
 %% Input: Let T be the tridiagonal symmetric matrix
 %% Output: Current matrix of T running unshifted QR Algorithm
 function Tnew = qralg(T)
-[m, ~] = size(T);
-while abs(T(m, m-1)) >= 10^(-12)
-    [Qk, Rk] = qr(T); % For greater efficiency
-    T = Rk * Qk; 
+    [m, ~] = size(T);
+    while abs(T(m, m-1)) >= 10^(-12)
+        [Qk, Rk] = qr(T); % For greater efficiency
+        T = Rk * Qk; 
+    end
+    Tnew = T; % output 
 end
-Tnew = T; % output 
