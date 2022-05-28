cc7baef30063821854235bc7e2089d054d416e19
Youngdo Lee <lyd0531@kaist.ac.kr>
2022-05-29 01:02:11 +0900

    Helper for Exercise 29 (b)
    Shifted QR Algorithm

---
+++ 29. QR Algorithm with Shifts/shftqralg_verb.m
@@ -0,0 +1,15 @@
+%% Input: Let T be the tridiagonal symmetric matrix
+%% Output: The eigenvalue of A, the vector storing abs(t_m,m-1),  
+%% This is the verbose version of shifted qralg.m .
+function [eigenvalue, logvector, Tnew] = shftqralg_verb(T)
+    [m, ~] = size(T);
+    logvector = [];
+    iter = 1;
+    while abs(T(m, m-1)) >= 10^(-12)
+        logvector(iter) = abs(A(m, m-1));
+        [Qk, Rk] = qr(T - (T(m, m) * eye(m, m))); % For greater efficiency
+        T = Rk * Qk + (T(m, m) * eye(m, m));
+        iter = iter + 1;
+    end
+    eigenvalue = T(m, m); logvector = logvector; Tnew = T;
+end
