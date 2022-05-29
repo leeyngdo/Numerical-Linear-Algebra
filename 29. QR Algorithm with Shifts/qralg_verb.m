5cfb539b49232d24d969cedd6f1aac62c20cf77b
Youngdo Lee <lyd0531@kaist.ac.kr>
2022-05-29 13:55:44 +0900

    Change the output order

--- 29. QR Algorithm with Shifts/qralg_verb.m
+++ 29. QR Algorithm with Shifts/qralg_verb.m
@@ -1,10 +1,10 @@
 %% Input: Let T be the tridiagonal symmetric matrix
 %% Output: The eigenvalue of Q of T's last column, the vector storing abs(t_m,m-1), and Tnew
 %% This is the verbose version of qralg.m .
-function [eigenvalue, logvector, Tnew] = qralg_verb(T)
+function [Tnew, eigenvalue, logvector] = qralg_verb(T)
     [m, ~] = size(T);
-    logvector = [];
-    iter = 1;
+    logvector = []; 
+    iter = 1; % Iteration step starts at 1 since MATLAB does not support zero index
     while abs(T(m, m-1)) >= 10^(-12)
         logvector(iter) = abs(T(m, m-1));
         [Qk, Rk] = qr(T); % For greater efficiency
